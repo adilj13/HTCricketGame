7ed0fcd5a9764a14ed822ff18c66d3dd6ff63734
ARazaMughal <ahmadraza89789@gmail.com>
2022-10-04 10:26:06 +0500

    overs

--- source.cpp
+++ source.cpp
@@ -2,7 +2,7 @@
 #include<ctime>
 #include<cstdlib>
 using namespace std;
-
+int totalover=0;
 bool toss();
 int compTurn();
 int userTurn();
@@ -11,10 +11,14 @@ void playagain();
 int main()
 {
 	cout << "Welcome to HTGame" << endl;
-	int userScore, compScore,tos ;
+	int userScore, compScore,tos,balls=1 ;
 	char again,tosss,choice;
 	do
 	{
+		cout<<"How Many overs do you want to play? ";
+		cin>>totalover;
+		cout<<"\t"<<totalover<<" Overs Match \n"<<endl;
+		
 		cout << "Select Head/Tales: (H/T) ";
 		cin >> tosss;
 		while (tosss != 'h' && tosss != 'H' && tosss != 't' && tosss != 'T')
@@ -87,9 +91,9 @@ int main()
 			cout << "\nWell Played\nMatch Tie." << endl;
 		}
 		playagain();
-	} while (again=='y'||again=='Y');
+	}
+	 while (again=='y'||again=='Y');
 	cout << "\n\nThanks for playing!\nMade By Adil Aziz."<<endl;
-	cin >> again;
 	return 0;
 }
 void playagain(){
@@ -108,48 +112,85 @@ int randNum()
 	num = 1 + rand() % 6;
 	return num;
 }
-
+int compscore=0,userscore=0;
 int compTurn()
 {
-	int userNum = 1, compNum = 0, score = 0;
-	cout << "\n\nComputer's Turn\n";
+	
+	int over=0,balls=0;
+	int userNum = 1, compNum = 0;
+	cout << "\tComputer's Turn\n";
 	while (compNum != userNum)
 	{
-		score += compNum;
+		balls=balls+1;
+		compscore += compNum;
 		compNum = randNum();
-		cout << "Your Number: ";
+		cout<<"\nTotal Score: "<<compscore<<endl;
+		cout<<"\nOver "<<over<<"."<<balls;
+		if (balls==6)
+		{
+			over=over+1;
+			balls=0;
+		}
+		if (over==totalover)
+		{
+			cout<<"\n\tOvers Ended";
+			break;
+		}
+		
+		
+		
+		cout << "\nYour Number: ";
 		cin >> userNum;
 		while (userNum>6 || userNum<1)
 		{
 			cout << "No ball! 1 score added to opponent's score";
-			score=score +1;
+			compscore=compscore +1;
 			cout<<"\nYour number: ";
 			cin >> userNum;
+
 		}
+		
 		cout << "Computer Number: " << compNum << endl;
 	}
-	cout << "\nComputer is out!\nComputer\'s Score is " << score << endl;
-	return score;
+	cout << "\nComputer's Inning Ended!\nComputer\'s Score is " << compscore << endl;
+	return compscore;
 }
 int userTurn()
 {
-	int userNum = 0, compNum = 1, score = 0;
-	cout << "\n\nYour Turn\n";
+	int over=0,balls=0;
+	int userNum = 0, compNum = 1;
+	cout << "\tYour Turn\n";
 	while (compNum != userNum)
 	{
-		score += userNum;
+		balls=balls+1;
+		userscore += userNum;
 		compNum = randNum();
-		cout << "Your Number: ";
+		cout<<"\nTotal Score: "<<userscore<<endl;
+		cout<<"\nOver "<<over<<"."<<balls;
+		if (balls==6)
+		{
+			over=over+1;
+			balls=0;
+		}
+		if (over==totalover)
+		{
+			cout<<"\n\tOvers Ended";
+			break;
+		}
+		cout << "\nYour Number: ";
 		cin >> userNum;
 		while (userNum>6 || userNum<1)
 		{
+			cout<<"Invalid number!\n";
 			cout << "Your Number: ";
 			cin >> userNum;
 		}
+		
 		cout << "Computer Number: " << compNum << endl;
+		
 	}
-	cout << "\nYou are Out!\nYour Score is " << score << endl;
-	return score;
+	cout << "\nYour Inning Ended!\nYour Score is " << userscore << endl;
+	return userscore;
 }
 bool toss()
 {
